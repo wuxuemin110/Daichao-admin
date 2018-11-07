509f96089b203d3598b8e999484162f7378a0897
吴学敏 <121774302@qq.com>
2018-11-07 11:26:29 +0800

    列表

--- src/views/modules/wxm/accountlist.vue
+++ src/views/modules/wxm/accountlist.vue
@@ -2,84 +2,102 @@
   <div class="mod-config">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
       <el-form-item>
-        <el-input v-model="dataForm.mobilePhone" placeholder="手机号码" clearable></el-input>
+        <el-input v-model="dataForm.productName" placeholder="产品名称查询" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="dataForm.productDisplayNum" placeholder="产品编号查询" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="日期" >
+        <el-date-picker
+          unlink-panels
+          v-model="dataForm.date"
+          type="datetimerange"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期">
+        </el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-      </el-form-item>
-      <el-form-item>
-        <el-button  v-if="isAuth('generation:userinfo:list')" type="primary">导出列表</el-button>
+
       </el-form-item>
     </el-form>
     <el-table
       :data="dataList"
       border
       v-loading="dataListLoading"
-
       style="width: 100%;">
 
       <el-table-column
-        prop="mobile"
+        prop="productDisplayNum"
         header-align="center"
         align="center"
-        label="用户">
+        label="产品编号">
       </el-table-column>
       <el-table-column
-        prop="channelName"
+        prop="productName"
+        header-align="center"
+        align="center"
+        label="产品名字">
+      </el-table-column>
+
+      <el-table-column
+        prop="price"
         header-align="center"
         align="center"
 
-        label="注册渠道">
+        label="单价">
       </el-table-column>
+
       <el-table-column
-        prop="deviceSource"
+        prop="balance"
         header-align="center"
         align="center"
-        label="设备">
+
+        label="余额">
       </el-table-column>
       <el-table-column
-        prop="market"
+        prop="clickComplete"
         header-align="center"
         align="center"
-        label="下载途径">
+
+        label="点击数">
       </el-table-column>
       <el-table-column
-        prop="appVersion"
+        prop="settedCount"
         header-align="center"
         align="center"
-        label="app版本">
+
+        label="结算金额">
       </el-table-column>
       <el-table-column
-        prop="appName"
+        prop="settedRegistered"
         header-align="center"
         align="center"
-        label="app名字">
+
+        label="结算注册数">
       </el-table-column>
+      <el-table-column
+        prop="conversionRate"
+        header-align="center"
+        align="center"
+        label="注册转化率">
+      </el-table-column>
+
       <el-table-column
         prop="createTime"
         header-align="center"
         align="center"
-        :formatter="dateFormat"
-        label="注册时间">
+        label="结算时间">
       </el-table-column>
       <el-table-column
-        prop="lastLoginTime"
+        prop="totalRecharge"
         header-align="center"
         align="center"
-        label="最近一次登陆时间">
+        label="总充值">
       </el-table-column>
-      <el-table-column
-        prop="loginCount"
-        header-align="center"
-        align="center"
-        label="总登陆次数">
-      </el-table-column>
-      <el-table-column
-        prop="clickCount"
-        header-align="center"
-        align="center"
-        label="点击次数">
-      </el-table-column>
+
     </el-table>
     <el-pagination
       @size-change="sizeChangeHandle"
@@ -90,43 +108,37 @@
       :total="totalPage"
       layout="total, sizes, prev, pager, next, jumper">
     </el-pagination>
-    <!-- 弹窗, 新增 / 修改 -->
-    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+
   </div>
 </template>
 
 <script>
-  import AddOrUpdate from './banner-add-or-update'
+
   import { formatDate } from '@/utils/format'
   export default {
     data () {
       return {
         dataForm: {
-          mobilePhone: ''
+          productName: null,
+          productDisplayNum:null,
+          date:null
         },
         dataList: [],
         pageIndex: 1,
         pageSize: 10,
         totalPage: 0,
         dataListLoading: false,
-
-        addOrUpdateVisible: false
+        addOrUpdateVisible: false,
+        dataListSelections:[]
       }
     },
     components: {
-      AddOrUpdate
+
     },
     activated () {
       this.getDataList()
     },
     methods: {
-      fromatAmount (row, column) {
-        var amount = row[column.property]
-        if (amount === undefined || amount == null) {
-          return ''
-        }
-        return amount / 100
-      },
       dateFormat (row, column) {
         var date = row[column.property]
         if (date === undefined || date == null) {
@@ -138,17 +150,21 @@
       getDataList () {
         this.dataListLoading = true
         this.$http({
-          url: this.$http.adornUrl(`/generation/userinfo/list`),
+          url: this.$http.adornUrl(`/generation/companyProduct/balance/list`),
           method: 'get',
           params: this.$http.adornParams({
+            'token': this.$cookie.get('token'),
             'page': this.pageIndex,
             'limit': this.pageSize,
-            'name': this.dataForm.name
+            'productName': this.dataForm.productName || null,
+            'productDisplayNum':this.dataForm.productDisplayNum || null,
+            'startDate':this.dataForm.date !== null ? this.dataForm.date[0] : null,
+            'endDate':this.dataForm.date !== null ? this.dataForm.date[1] : null,
           })
         }).then(({data}) => {
           if (data && data.code === 0) {
-            this.dataList = data.page.list
-            this.totalPage = data.page.totalCount
+            this.dataList = data.companyProductBalanceVOs
+            this.totalPage = data.totalCount
           } else {
             this.dataList = []
             this.totalPage = 0
@@ -167,14 +183,44 @@
         this.pageIndex = val
         this.getDataList()
       },
+
       // 新增 / 修改
-      addOrUpdateHandle (id) {
+      addOrUpdateHandle (item) {
         this.addOrUpdateVisible = true
         this.$nextTick(() => {
-          this.$refs.addOrUpdate.init(id)
+          this.$refs.addOrUpdate.init(item)
+        })
+      },
+      // 删除
+      deleteHandle (id) {
+        this.$confirm(`确定要删除吗?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/generation/companyInfo/delete'),
+            method: 'post',
+            params: this.$http.adornParams({
+              'token':this.$cookie.get('token'),
+              'companyId':id,
+            })
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
         })
       }
-
     }
   }
 </script>
