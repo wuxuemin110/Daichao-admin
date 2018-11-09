0086398c1bcd8c3ea271f05ac038a4eaa54af2a3
monoshizuka <814274811@qq.com>
2018-11-09 17:43:43 +0800

    取消

---
+++ static/config/index.js
@@ -0,0 +1,18 @@
+/**
+ * 开发环境
+ */
+;(function () {
+  window.SITE_CONFIG = {}
+
+  // api接口请求地址
+
+    window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.117:8080';
+
+  // window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.123:8080'; //唐杰
+  // window.SITE_CONFIG['baseUrl'] = 'http://juzi.liulianggame.com:8080/loan-server'
+
+  // cdn地址 = 域名 + 版本号
+  window.SITE_CONFIG['domain'] = './' // 域名
+  window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
+  window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version
+})()
