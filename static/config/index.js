acd9279caac0c621fc9c8d655eb9d85c54da2d15
吴学敏 <121774302@qq.com>
2018-11-09 16:18:41 +0800

    banner修改

--- static/config/index.js
+++ static/config/index.js
@@ -5,12 +5,12 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  
-    //window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.117:8080';
+
+    window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.117:8080';
 
 
-  window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.123:8080'; //唐杰
-  // window.SITE_CONFIG['baseUrl'] = 'http://juzi.liulianggame.com:8080/loan-server';
+ // window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.123:8080'; //唐杰
+   //window.SITE_CONFIG['baseUrl'] = 'http://juzi.liulianggame.com:8080/loan-server';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
