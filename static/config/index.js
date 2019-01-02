094a27070b92eb13b128a1d2cc60c16d3081bf19
吴学敏 <121774302@qq.com>
2019-01-02 09:39:41 +0800

    first commit

--- static/config/index.js
+++ static/config/index.js
@@ -8,11 +8,6 @@
 
   window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.118:8080';
 
-
-  // window.SITE_CONFIG['baseUrl'] = ''; //唐杰
-  //   window.SITE_CONFIG['baseUrl'] = '';
-
-
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
